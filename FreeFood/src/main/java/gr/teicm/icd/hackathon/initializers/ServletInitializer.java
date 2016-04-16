6be30dbcef3024d68343ec8027994556058c6f59
proro <proro@DESKTOP-DI580GP>
2016-04-16 12:00:20 +0300

    ServletInitializer override

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/initializers/ServletInitializer.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/initializers/ServletInitializer.java
@@ -3,8 +3,6 @@ package gr.teicm.icd.hackathon.initializers;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
-import gr.teicm.icd.hackathon.FreeFoodApplication;
-
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
