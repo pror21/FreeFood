8b20304c813f570b0d0808d2d3ce1d35d78f1bd2
proro <proro@DESKTOP-DI580GP>
2016-04-16 13:30:36 +0300

    deleteFoodMark() on run()

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/schedulers/FoodMarkScheduler.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/schedulers/FoodMarkScheduler.java
@@ -2,11 +2,21 @@ package gr.teicm.icd.hackathon.schedulers;
 
 import java.util.TimerTask;
 
+import gr.teicm.icd.hackathon.models.FoodMark;
+import gr.teicm.icd.hackathon.services.FoodMarkService;
+
 public class FoodMarkScheduler extends TimerTask {
+	
+	private FoodMark foodmark;
+	private FoodMarkService markFoodService;
+	
+	public FoodMarkScheduler(FoodMarkService markFoodService, FoodMark foodmark) {
+		this.markFoodService = markFoodService;
+		this.foodmark = foodmark;
+	}
 	
 	@Override
 	public void run() {
-		
+		markFoodService.deleteFoodMark(foodmark);
 	}
-
 }
