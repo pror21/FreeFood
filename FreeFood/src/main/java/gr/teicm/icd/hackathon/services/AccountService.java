9e71de1f4f3677a5772bf073043e4edc96d16949
proro <proro@DESKTOP-DI580GP>
2016-04-16 13:30:59 +0300

    insertAccount() added

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/services/AccountService.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/services/AccountService.java
@@ -1,37 +1,28 @@
 package gr.teicm.icd.hackathon.services;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import gr.teicm.icd.hackathon.configs.SpringMongoConfig;
 import gr.teicm.icd.hackathon.models.Account;
+import gr.teicm.icd.hackathon.repositories.AccountRepository;
 
 @Service
 public class AccountService {
+	
+	@Autowired
+	private AccountRepository accountRepository;
 	
 	public AccountService() {
 
 	}
 	
 	public boolean insertAccount(Account account) {
-		@SuppressWarnings("resource")
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
-		MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
-
-		Query query = new Query();
-		query.addCriteria(Criteria.where("accUsername").is(account.getUsername()));
-
-		if(mongoOperation.exists(query, Account.class)){
-			return false;
-		}
-		else {
+		if(accountRepository.findByUsername(account.getUsername()) == null) {
 			account.addRole("ROLE_USER");
-			mongoOperation.insert(account);
+			accountRepository.insert(account);
 			return true;
 		}
+		else
+			return false;
 	}
 }
