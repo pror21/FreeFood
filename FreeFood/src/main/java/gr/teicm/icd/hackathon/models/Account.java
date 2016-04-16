72a7fef57a35a161381b5875e57bc916635d41e5
proro <proro@DESKTOP-DI580GP>
2016-04-16 10:19:55 +0300

    Account Entity Model

---
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/models/Account.java
@@ -0,0 +1,87 @@
+package gr.teicm.icd.hackathon.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "accounts")
+public class Account {
+
+	public enum Type {
+		INDIVIDUAL, STORE, FOUNDATION, OTHER;
+	}
+
+	@Id private String id;
+	private String username;
+	private String name;
+	private String email;
+	private String password;
+	private String about;
+	private Type type;
+	private double latitude;
+	private double longitude;
+	private Set<String> roles = new HashSet<String>();
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAbout() {
+		return about;
+	}
+	public void setAbout(String about) {
+		this.about = about;
+	}
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public Set<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
+	}
+}
