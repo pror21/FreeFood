5278432b99ba332085be0591dbad502c77b61441
proro <proro@DESKTOP-DI580GP>
2016-04-16 10:18:41 +0300

    Home Controller

---
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package gr.teicm.icd.hackathon.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = { "", "/", "/index" })
+	public String index(Model model) {
+		return "index";
+	}
+}
