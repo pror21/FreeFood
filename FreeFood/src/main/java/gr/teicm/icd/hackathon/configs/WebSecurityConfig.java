ec07cca5b0e521552b3ea161e92d192b606d101a
proro <proro@DESKTOP-DI580GP>
2016-04-16 11:21:49 +0300

    Authenticate with CustomAccountDetailsService

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/configs/WebSecurityConfig.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/configs/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package gr.teicm.icd.hackathon.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import gr.teicm.icd.hackathon.services.CustomAccountDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +16,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/index", "/index.html", "/css/**", "/js/**", "/fragments/**,")
+				.antMatchers(
+						"/", 
+						"/index", 
+						"/index.html", 
+						"/register",
+						"registerDone",
+						"registerFailed",
+						"/css/**", 
+						"/js/**", 
+						"/img/**", 
+						"/fragments/**"
+						)
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
 				.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/");
+	}
+	
+	@Autowired
+    private CustomAccountDetailsService customAccountDetailsService;
+	
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.userDetailsService(customAccountDetailsService);
 	}
 }
