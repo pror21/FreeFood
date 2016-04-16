7be64d863c209a63674e91140f7aea307d1a3878
proro <proro@DESKTOP-DI580GP>
2016-04-16 16:42:52 +0300

    AccountController work

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/AccountController.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/AccountController.java
@@ -1,5 +1,8 @@
 package gr.teicm.icd.hackathon.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,19 +11,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import gr.teicm.icd.hackathon.models.Account;
 import gr.teicm.icd.hackathon.models.FoodMark;
+import gr.teicm.icd.hackathon.repositories.AccountRepository;
+import gr.teicm.icd.hackathon.services.AccountService;
+import gr.teicm.icd.hackathon.services.FoodMarkService;
 
 @Controller
 public class AccountController {
 
+	@Autowired
+	private AccountService accountService;
+	
+	@Autowired
+	private FoodMarkService foodMarkService;
+	
+	@Autowired
+	private AccountRepository accountRepository;
+	
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String register(Model model) {
 		model.addAttribute("account", new Account());
+		
 		return "register";
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String insertAccount(@ModelAttribute Account account, Model model) {
-		return "registerDone";
+		model.addAttribute("account", account);
+		boolean registered = accountService.insertAccount(account);
+		if (registered == true)
+			return "registerDone";
+		else
+			return "registerFailed";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -31,12 +52,21 @@ public class AccountController {
 	@RequestMapping(value = "/donate", method = RequestMethod.GET)
 	public String donate(Model model) {
 		model.addAttribute("foodmark", new FoodMark());
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Account account = new Account();
+		account = accountRepository.findByUsername(auth.getName());
+		model.addAttribute("user", account);
 		return "donate";
 	}
 
 	@RequestMapping(value = "/donate", method = RequestMethod.POST)
 	public String insertDonate(@ModelAttribute FoodMark foodmark, Model model) {
-		return "insertDone";
+		model.addAttribute("foodmark", foodmark);
+		boolean inserted = foodMarkService.insertFoodMark(foodmark);
+		if (inserted == true)
+			return "listingDone";
+		else
+			return "listingFailed";
 	}
 
 	@RequestMapping(value = "/listingDone", method = RequestMethod.GET)
