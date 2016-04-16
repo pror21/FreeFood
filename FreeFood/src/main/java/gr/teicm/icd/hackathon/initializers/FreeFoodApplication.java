9813ea2504bbe4aa929050f88577bb01420f2013
proro <proro@DESKTOP-DI580GP>
2016-04-16 16:42:01 +0300

    @ComponentScan added

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/initializers/FreeFoodApplication.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/initializers/FreeFoodApplication.java
@@ -2,7 +2,9 @@ package gr.teicm.icd.hackathon.initializers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("gr.teicm.icd.hackathon")
 @SpringBootApplication
 public class FreeFoodApplication {
 
