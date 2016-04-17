3aa5b5e9841b3defcde410fbec182377d69f07af
proro <proro@DESKTOP-DI580GP>
2016-04-17 14:52:02 +0300

    Major fixes

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/AccountController.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/AccountController.java
@@ -1,8 +1,8 @@
 package gr.teicm.icd.hackathon.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import gr.teicm.icd.hackathon.models.Account;
 import gr.teicm.icd.hackathon.models.FoodMark;
 import gr.teicm.icd.hackathon.repositories.AccountRepository;
+import gr.teicm.icd.hackathon.repositories.FoodMarkRepository;
 import gr.teicm.icd.hackathon.services.AccountService;
 import gr.teicm.icd.hackathon.services.FoodMarkService;
 
@@ -27,10 +28,12 @@ public class AccountController {
 	@Autowired
 	private AccountRepository accountRepository;
 	
+	@Autowired
+	private FoodMarkRepository foodMarkRepository;
+	
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String register(Model model) {
 		model.addAttribute("account", new Account());
-		
 		return "register";
 	}
 
@@ -50,13 +53,18 @@ public class AccountController {
 	}
 
 	@RequestMapping(value = "/donate", method = RequestMethod.GET)
-	public String donate(Model model) {
+	public String donate(Model model, Principal principal) {
 		model.addAttribute("foodmark", new FoodMark());
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		Account account = new Account();
-		account = accountRepository.findByUsername(auth.getName());
-		model.addAttribute("user", account);
-		return "donate";
+		Account account = accountRepository.findByUsername(principal.getName());
+		FoodMark foodMark = foodMarkRepository.findByAccId(account.getId());
+		
+		if (foodMark != null) {
+			return "redirect:listing";
+		}
+		else {
+			model.addAttribute("user", account);
+			return "donate";
+		}
 	}
 
 	@RequestMapping(value = "/donate", method = RequestMethod.POST)
@@ -90,7 +98,18 @@ public class AccountController {
 	}
 
 	@RequestMapping(value = "/listing", method = RequestMethod.GET)
-	public String listing() {
+	public String listing(Model model, Principal principal) {
+		Account account = new Account();
+		account = accountRepository.findByUsername(principal.getName());
+		FoodMark foodmark = new FoodMark();
+		foodmark = foodMarkRepository.findByAccId(account.getId());
+		model.addAttribute("foodmark", foodmark);
 		return "listing";
+	}
+	
+	@RequestMapping(value = "/listing", method = RequestMethod.POST)
+	public String listingUpdate(@ModelAttribute FoodMark foodmark) {
+		foodMarkService.updateFoodMarkListing(foodmark);
+		return "redirect:/";
 	}
 }
--- FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/HomeController.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/controllers/HomeController.java
@@ -1,14 +1,22 @@
 package gr.teicm.icd.hackathon.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import gr.teicm.icd.hackathon.services.FoodMarkService;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private FoodMarkService foodMarkService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
+		model.addAttribute("allFoodMarks", foodMarkService.getAllFoodMarks());
 		return "index";
 	}
 }
