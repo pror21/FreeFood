75f755af939fc787dbf2584e90f9b18736278f8a
proro <proro@DESKTOP-DI580GP>
2016-04-17 14:55:25 +0300

    Use of @Query annotation & clean up

--- FreeFood/src/main/java/gr/teicm/icd/hackathon/repositories/FoodMarkRepository.java
+++ FreeFood/src/main/java/gr/teicm/icd/hackathon/repositories/FoodMarkRepository.java
@@ -1,17 +1,14 @@
 package gr.teicm.icd.hackathon.repositories;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import gr.teicm.icd.hackathon.models.Account;
 import gr.teicm.icd.hackathon.models.FoodMark;
 
 @Repository
 public interface FoodMarkRepository extends MongoRepository<FoodMark, String> {
 
-	FoodMark findByAcc(Account account);
-	List<FoodMark> findAll();
-	
+	@Query("{ 'acc.id' :  ?0 }")
+	FoodMark findByAccId(String id);
 }
